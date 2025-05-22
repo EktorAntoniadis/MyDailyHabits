3289a0779328c32990db5ef2e7d4c36ecafacd77
EktorAntoniadis <117900460+EktorAntoniadis@users.noreply.github.com>
2025-05-22 16:58:36 +0300

    προσθήκη κλάσης για έλεγχο κωδικού

---
+++ MyDailyHabits.Operations/Utils/SimplePasswordValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDailyHabits.Operations.Utils
+{
+    public class SimplePasswordValidator
+    {
+        public List<string> Validate(string password)
+        {
+            var errors = new List<string>();
+
+            if (!password.Any(char.IsUpper))
+            {
+                errors.Add("Password must contain at least one uppercase letter.");
+
+            }
+
+            if (!password.Any(char.IsLower))
+            {
+                errors.Add("Password must contain at least one lowercase letter.");
+
+            }
+
+            if (!password.Any(char.IsDigit))
+            {
+                errors.Add("Password must contain at least one number.");
+            }
+
+            if (!password.Any(ch => !char.IsLetterOrDigit(ch)))
+            {
+                errors.Add("Password must contain at least one special character (e.g., !, @, #).");
+
+            }
+
+            return errors;
+        }
+    }
+}
